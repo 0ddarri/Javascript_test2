eeaed93729cde3252debc32cd8e99e4cb363130e
0ddarri <eungania88@gmail.com>
2020-12-17 20:54:09 +0900

    d

--- Chapter01/C0101/sketchC0101.js
+++ Chapter01/C0101/sketchC0101.js
@@ -1,18 +1,25 @@
-<reference path="../../lib/p5.global-mode.d.ts"/>
+/// <reference path="../../lib/p5.global-mode.d.ts"/>
 
 function setup()
 {
     createCanvas(800, 600);
-    background(96, 96, 96);
+    //background(96, 96, 96);
 }
 
 function draw()
 {
-    //background(96, 96, 96);
-    
-}
+    background(96, 96, 96);
 
-function mousePressed()
-{
-    circle(mouseX, mouseY, 10);
+    textSize(20);
+    noStroke();
+    fill(255, 0, 0);
+    text(parseInt(mouseX), 0, mouseY);
+    stroke(255, 0, 0);
+    line(0, mouseY, windowWidth, mouseY);
+    
+    noStroke();
+    fill(0, 255, 0);
+    text(parseInt(mouseY), mouseX, 30);
+    stroke(0, 255, 0);
+    line(mouseX, 0, mouseX, windowHeight);
 }
--- Chapter01/C0102/sketch.js
+++ Chapter01/C0102/sketch.js
@@ -1,18 +1,22 @@
-<reference path="../../lib/p5.global-mode.d.ts"/>
+/// <reference path="../../lib/p5.global-mode.d.ts"/>
 
 function setup()
 {
     createCanvas(800, 600);
-    //background(96, 96, 96);
+    background(50, 50, 50);
 }
 
 function draw()
 {
-    background(90);
-    
+    //background(90);
+
+    line(400, 300, mouseX, mouseY);
+
+    fill(random(0, 255), random(0, 255), random(0, 255));
+    circle(mouseX, mouseY, 30);
 }
 
 function mousePressed()
 {
-    circle(mouseX, mouseY, 10);
+    background(50, 50, 50);
 }
