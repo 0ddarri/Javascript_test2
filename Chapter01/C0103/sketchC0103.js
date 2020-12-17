1e7cf3c55e9c954478fa09909cf66e1844bbdd6b
0ddarri <eungania88@gmail.com>
2020-12-17 21:06:07 +0900

    fix

--- Chapter01/C0103/sketchC0103.js
+++ Chapter01/C0103/sketchC0103.js
@@ -14,6 +14,6 @@ function draw()
 function mousePressed()
 {
     stroke(random(0, 255), random(0, 255), random(0, 255));
-    fill(random(0, 255), random(0, 255), random(0, 255));1
+    fill(random(0, 255), random(0, 255), random(0, 255));
     square(random(50, 750), random(50, 550), random(10, 50));
 }
