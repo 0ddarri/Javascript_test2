df0faafa53228e419a995eb71c5b0daee9500229
0ddarri <eungania88@gmail.com>
2020-12-17 20:25:31 +0900

    s

---
+++ Chapter01/C0101/sketchC0101.js
@@ -0,0 +1,18 @@
+<reference path="../../lib/p5.global-mode.d.ts"/>
+
+function setup()
+{
+    createCanvas(800, 600);
+    background(96, 96, 96);
+}
+
+function draw()
+{
+    //background(96, 96, 96);
+    
+}
+
+function mousePressed()
+{
+    circle(mouseX, mouseY, 10);
+}
